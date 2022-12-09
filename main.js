a8e6660ebef8529171ef7b6908dfb4f1fb4ec1a3
Sakshi Nayan <sakshi12princi@gmail.com>
2022-12-09 14:03:07 +0530

    done

--- main.js
+++ main.js
@@ -1,85 +1,126 @@
-console.log('My Tic-tac')
-//Selecting starting page
-const selectBox = document.querySelector('.select-box'),
-choose = document.querySelectorAll(".choose")
+//selecting all required elements
+const selectBox = document.querySelector(".select-box"),
+selectBtnX = selectBox.querySelector(".options .playerX"),
+selectBtnO = selectBox.querySelector(".options .playerO"),
+playBoard = document.querySelector(".play-board"),
+players = document.querySelector(".players"),
+allBox = document.querySelectorAll("section span"),
+resultBox = document.querySelector(".result-box"),
+wonText = resultBox.querySelector(".won-text"),
+replayBtn = resultBox.querySelector("button");
+let gameWin = new Audio("game_win.mp3");
+let gameover = new Audio("game_over.mp3");
 
-//selecting game page
-const gameZone = document.querySelector("#game-container"),
-showChange = document.querySelector(".slider")
 
-//how can we change turn
-let changeTurn =null
+window.onload = ()=>{ 
+    for (let i = 0; i < allBox.length; i++) {
+       allBox[i].setAttribute("onclick", "clickedBox(this)");
+    }
+}
 
-choose.forEach(choosePlayer =>{
-    choosePlayer.addEventListener('click', ()=>{
-        if(choosePlayer.id === 'playerX'){
-            changeTurn = false;
-            console.log(changeTurn)
-            showChange.style.left =`160px`;
-        }else{
-            changeTurn = true;
-            console.log(changeTurn)
-            showChange.style.left =`160px`;
+selectBtnX.onclick = ()=>{
+    selectBox.classList.add("hide");
+    playBoard.classList.add("show"); 
+}
+
+selectBtnO.onclick = ()=>{ 
+    selectBox.classList.add("hide");
+    playBoard.classList.add("show"); 
+    players.setAttribute("class", "players active player"); 
+}
+
+let playerXIcon = "fas fa-times"; 
+let playerOIcon = "far fa-circle"; 
+let playerSign = "X"; 
+let runBot = true; 
+
+function clickedBox(element){
+    if(players.classList.contains("player")){
+        playerSign = "O"; 
+        element.innerHTML = `<i class="${playerOIcon}"></i>`; 
+        players.classList.remove("active"); 
+        element.setAttribute("id", playerSign); 
+    }else{
+        element.innerHTML = `<i class="${playerXIcon}"></i>`; 
+        element.setAttribute("id", playerSign); 
+        players.classList.add("active"); 
+    }
+    selectWinner(); 
+    element.style.pointerEvents = "none"; 
+    playBoard.style.pointerEvents = "none"; 
+    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed(); 
+    setTimeout(()=>{
+        bot(runBot); 
+    }, randomTimeDelay); 
+}
+
+// bot auto select function
+function bot(){
+    let array = []; 
+    if(runBot){ 
+        playerSign = "O"; 
+        for (let i = 0; i < allBox.length; i++) {
+            if(allBox[i].childElementCount == 0){ 
+                array.push(i); 
+            }
         }
-        selectBox.style.display ="none";
-        gameZone.style.display ="block";
-        // selectBox.classList.add("hide");
-        // gameZone.classList.add("show")
-    })
-})
+        let randomBox = array[Math.floor(Math.random() * array.length)]; 
+        if(array.length > 0){ 
+            if(players.classList.contains("player")){ 
+                playerSign = "X"; 
+                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; 
+                allBox[randomBox].setAttribute("id", playerSign); 
+                players.classList.add("active"); 
+            }else{
+                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; 
+                players.classList.remove("active"); 
+                allBox[randomBox].setAttribute("id", playerSign); 
+            }
+            selectWinner(); 
+        }
+        allBox[randomBox].style.pointerEvents = "none"; 
+        playBoard.style.pointerEvents = "auto"; 
+        playerSign = "X";
+    }
+}
 
+function getIdVal(classname){
+    return document.querySelector(".box" + classname).id; 
+}
+function checkIdSign(val1, val2, val3, sign){ 
+    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
+        return true;
+    }
+}
+function selectWinner(){ 
+    if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
+        runBot = false; 
+        bot(runBot); 
+        setTimeout(()=>{ 
+            resultBox.classList.add("show");
+            playBoard.classList.remove("show");
+            document.querySelector(".imageBox").getElementsByTagName("img")[0].style.width="200px"
+        }, 700);
+        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`; 
+        gameWin.play();
+        // wingif.style.display="block";
+        
+    }else{
+        if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
+            runBot = false; 
+            bot(runBot); 
+            setTimeout(()=>{ 
+                resultBox.classList.add("show");
+                playBoard.classList.remove("show");
+                document.querySelector(".imageBox").getElementsByTagName("img")[1].style.width="200px"
+            }, 700);
+            wonText.textContent = "Match has been drawn!";
+            gameover.play(); 
 
+        }
+    }
+}
 
-
-
-// let gameWin = new Audio("game_win.mp3");
-// let gameover = new Audio("game_over.mp3");
-// let audioTurn = new Audio('ui-click-97915.mp3')
-
-
-// let turn="X"
-// let isgameOver = false
-
-// //function to change the turn
-// const ChangeTrun=()=>{
-//     return turn === "X"? "0" : "X"
-// }
-
-// //function to check for win
-// const checkWin =()=>{
-//     let wins =[
-//         [0, 1, 2],
-//         [3, 4 ,5],
-//         [6 ,7 ,8],
-//         [0 ,3 ,6],
-//         [1 ,4 ,7],
-//         [2 ,5 ,8],
-//         [0 ,4 ,8],
-//         [2 ,4 ,6]
-//     ]
-//     wins.forEach(e =>{
-//         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
-//             document.querySelector(".info").innerText = boxtext[e[0]].innerText + 'Won';
-//             // gameWin.play();
-//             isgameOver = true
-//         }
-//     })
-// }
-
-// //Game Logic
-// let boxes= document.getElementsByClassName('box');
-// Array.from(boxes).forEach(element =>{
-//     let boxtext = element.querySelector('.boxText');
-//     element.addEventListener('click',(e)=>{
-//         if(boxtext.innerText === ''){
-//             boxtext.innerText = turn;
-//             turn = ChangeTrun();
-//             checkWin();
-//             audioTurn.play();
-//             if(!isgameOver){
-//                 document.getElementsByClassName('info')[0].innerText ='Turn for' +" "+ turn;
-//             }
-            
-//         }
-//     })
-// })
+replayBtn.onclick = ()=>{
+    window.location.reload(); 
+}
